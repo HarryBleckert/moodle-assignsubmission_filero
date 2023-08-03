f8397bf3d0414675f050d0d2b12c8a24db21dfd4
Harry Bleckert <Harry@Bleckert.com>
2023-08-03 19:18:37 +0800

    none

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023073101;
+$plugin->version = 2023080301;
 $plugin->requires = 2021111800;
 $plugin->component = 'assignsubmission_filero';
