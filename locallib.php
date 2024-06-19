33a00711c93c292c1c03f07d77862da3746c7094
Harry Bleckert <Harry@Bleckert.com>
2024-06-19 23:26:38 +0800

    no comments

--- locallib.php
+++ locallib.php
@@ -346,6 +346,8 @@ class assign_submission_filero extends assign_submission_plugin {
             if (!$this->check_enrolment($assignment->course, $submission->userid)){
                 continue;
             }
+            assignsubmission_filero_observer::observer_log("grader_submissions: Assignments loop: "
+                    ."Submission $destsubmission->id for assignment " .$assignment->name." (".$assignment->id . ") from user id $submission->userid");
             if ( isset($destsubmission->id)) {
                 if ( $action == "revert" OR $action == "remove") {
                     if (!$coursemodule = get_coursemodule_from_instance('assign', $destsubmission->assignment)) {
