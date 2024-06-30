4505c29b62af2cd2be17f37bc2c218058028303c
Harry Bleckert <Harry@Bleckert.com>
2024-07-01 00:57:49 +0800

    no comments

--- locallib.php
+++ locallib.php
@@ -407,12 +407,16 @@ class assign_submission_filero extends assign_submission_plugin {
                         . $destsubmission->id . " of assignment " . $assignment->name . " created from submission "
                         . $currentsubmission->id . " of assignment " . $assignmentname);
 
+
                 if (!$coursemodule = get_coursemodule_from_instance('assign', $destsubmission->assignment)) {
                     assignsubmission_filero_observer::observer_log(
                             "grader_submissions(): Course Module not found for submission $destsubmission->id of assignment $assignment->name!");
                     continue;
                 }
-                $assign->notify_graders( $destsubmission);
+                $coursemodulecontext = context_module::instance($coursemodule->id);
+                $assign_g = new assign($coursemodulecontext, $coursemodule, $assignment->course);
+                $assign_g->notify_graders( $destsubmission);
+
 
                 /* disabled Nov 15,2023 on DHBW demand not to archive duplicate submission files
                 $_SESSION['filero_submit_for_grading_' . $destsubmission->id] = true;
