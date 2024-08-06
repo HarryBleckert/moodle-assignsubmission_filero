a799b2c02c4a241e3143f644dffcf0733c50a184
Harry Bleckert <Harry@Bleckert.com>
2024-08-06 15:53:28 +0800

    no comments

--- locallib.php
+++ locallib.php
@@ -424,7 +424,7 @@ class assign_submission_filero extends assign_submission_plugin {
                 }
                 $search=shell_exec( '/usr/bin/grep "function notify_graders" ' .$CFG->dirroot. '/mod/assign/locallib.php');
                 assignsubmission_filero_observer::observer_log("Search: $search");
-                if (!stristr( $search, "protected")) {
+                if (!stristr( $search, "protected")) {*/
                     if (!$coursemodule = get_coursemodule_from_instance('assign', $destsubmission->assignment)) {
                         assignsubmission_filero_observer::observer_log(
                                 "grader_submissions(): Course Module not found for submission $destsubmission->id of assignment $assignment->name!");
@@ -432,17 +432,17 @@ class assign_submission_filero extends assign_submission_plugin {
                     }
                     $coursemodulecontext = context_module::instance($coursemodule->id);
                     $assign = new assign($coursemodulecontext, $coursemodule, $assignment->course);
-                    $assign->notify_graders($destsubmission);
+                    $assign::notify_graders($destsubmission);
                     assignsubmission_filero_observer::observer_log("grader_submissions: notify_graders: - "
                             . $destsubmission->id . " of assignment " . $assignment->name . " created from submission "
                             . $currentsubmission->id . " of assignment " . $assignmentname);
-                }
+                /*}
                 else{
                     assignsubmission_filero_observer::observer_log("ERROR: grader_submissions: notify_graders can't be called. The function is protected. - "
                             . $destsubmission->id . " of assignment " . $assignment->name . " created from submission "
                             . $currentsubmission->id . " of assignment " . $assignmentname);
-                }
-                */
+                }*/
+
 
 
                 /* disabled Nov 15,2023 on DHBW demand not to archive duplicate submission files
